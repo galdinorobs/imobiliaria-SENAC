c37401a9f5fa7c2d78bdb958bd93e18ed607b80a
Robson <jrobson.santos81@gmail.com>
2024-06-13 19:00:38 -0300

    fix: corrige erro de conexão com o banco de dados

--- cadastrar.php
+++ cadastrar.php
@@ -10,6 +10,7 @@ if(!$conn) {
     die("Conexão falhou:".mysqli_connect_erro());
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $titulo = mysqli_real_escape_string($conn,$_POST['titulo']);
 $descricao = mysqli_real_escape_string($conn,$_POST['descricao']);
 $categoria = mysqli_real_escape_string($conn,$_POST['categoria']);
@@ -22,6 +23,7 @@ if (mysqli_query($conn,$sql)) {
     echo "Imóvel cadastrado com sucesso!";
 } else {
     echo "Erro: " . $sql . "<br>" .mysqli_error($conn);
+    }
 }
     mysqli_close($conn);
 ?>
@@ -48,22 +50,22 @@ if (mysqli_query($conn,$sql)) {
     </header>    
     <main>        
         <section>            
-            <form method="POST">                
+            <form method="POST" enctype="multipart/form-data">                
                 <label for="titulo">Título do Anúncio:</label>                
                 <input type="text" id="titulo" name="titulo" required>                                
                 <label for="descricao">Descrição do Imóvel:</label>                
                 <textarea id="descricao" name="descricao" required></textarea>                                
                 <label for="categoria">Tipo de Imóvel:</label>                
-                <select id="categoria" name="categoria" required>                    
-                    <option value="casa">Casa</option>                    
-                    <option value="apartamento">Apartamento</option>                    
-                    <option value="terreno">Terreno</option>                
-                </select>                                
+                    <select id="categoria" name="categoria" required>                    
+                        <option value="casa">Casa</option>                    
+                        <option value="apartamento">Apartamento</option>                    
+                        <option value="terreno">Terreno</option>                
+                    </select>                                
                 <label for="tipo">Tipo de Negócio:</label>                
-                <select id="tipo" name="tipo" required>                    
-                    <option value="venda">Venda</option>                    
-                    <option value="aluguel">Aluguel</option>                
-                </select>                                
+                    <select id="tipo" name="tipo" required>                    
+                        <option value="venda">Venda</option>                    
+                        <option value="aluguel">Aluguel</option>                
+                    </select>                                
                 <label for="preco">Preço do Imóvel:</label>                
                 <input type="number" id="preco" name="preco" required>                                
                 <button type="submit">Cadastrar Imóvel</button>            
